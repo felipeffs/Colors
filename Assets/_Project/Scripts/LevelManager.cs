58484121733d06ae255e38de9da53fc96c4008cd
Felipe <felipe8ffs@gmail.com>
2023-01-18 08:15:31 -0300

    Game Handle was absorbed into Level Manager

--- Assets/_Project/Scripts/LevelManager.cs
+++ Assets/_Project/Scripts/LevelManager.cs
@@ -6,8 +6,14 @@ public class LevelManager : MonoBehaviour
 {
     public static event Action OnRestartLevel;
 
+    //
     [SerializeField] private TextMeshProUGUI restartText;
     [SerializeField] private Transform startPoint;
+
+    //Player
+    [SerializeField] private GameObject bitPrefab;
+    private GameObject _bit;
+    private BitController _bitController;
 
     private void Start()
     {
@@ -31,7 +37,7 @@ public class LevelManager : MonoBehaviour
     private void RestartLevel()
     {
         restartText.enabled = false;
-        GameHandler.Instance.ResetBit(startPoint.position);
+        ResetBit(startPoint.position);
         OnRestartLevel?.Invoke();
     }
 
@@ -39,4 +45,18 @@ public class LevelManager : MonoBehaviour
     {
         restartText.enabled = true;
     }
+
+    public void ResetBit(Vector3 newPosition)
+    {
+        if (_bit == null)
+        {
+            _bit = Instantiate(bitPrefab, Vector3.zero, bitPrefab.transform.rotation);
+            _bitController = _bit.GetComponentInChildren<BitController>();
+            _bit.SetActive(false);
+        }
+
+        _bitController.Reset(newPosition);
+        _bit.SetActive(true);
+    }
 }
+
