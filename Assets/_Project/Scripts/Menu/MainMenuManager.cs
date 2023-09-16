798dc1fc9d358246e00809311c74214c1975077f
Felipe <felipe8ffs@gmail.com>
2023-09-16 08:45:26 -0300

    Settings Menu Part 1
    + Main navigation working
    + Improvement in the ButtonPlus component
    + Basic operation of the display mode option (only on the controller for now)

--- Assets/_Project/Scripts/Menu/MainMenuManager.cs
+++ Assets/_Project/Scripts/Menu/MainMenuManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private SOLevelOrder levelOrder;
+    [SerializeField] private ButtonPlus settingsInitialButton;
 
     public void CloseGame()
     {
@@ -12,5 +13,12 @@ public class MainMenuManager : MonoBehaviour
     public void StartGame()
     {
         levelOrder.LoadLevelByIndex(0);
+    }
+
+    public void SettingsMenu()
+    {
+        gameObject.SetActive(false);
+        UIPlusModule.Instance.ChangeInitialSelectedButton(settingsInitialButton);
+        UIPlusModule.Instance.SetSelected(settingsInitialButton);
     }
 }
